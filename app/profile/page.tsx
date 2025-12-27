25f4f48fabcd82b3795bd234e8cae30cf663599f
sahil170602 <sahilmeshram1917@.com>
2025-12-27 20:20:24 +0530

    Fix build error: force dynamic profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Navbar from '@/components/Navbar';
 import BackButton from '@/components/BackButton';
 import { PrismaClient } from '@prisma/client';
